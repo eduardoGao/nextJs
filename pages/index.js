f069d90ad9c53aa2fd63a1273331e03767003a36
Lalo Diaz <lalo.diaz1896@gmail.com>
2020-10-12 23:23:51 -0500

    Basic Config in NextJs

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+// Componente de React que trabaje como una página
+
+import React from 'react';
+
+const Home = () => {
+    return(
+        <div>
+            <h1>Hola Next JS</h1>
+        </div>
+    );
+};
+
+export default Home;
